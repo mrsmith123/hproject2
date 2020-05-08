f00023d96489387b04e222d0d9104bc710e5c786
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-05-08 19:25:09 +0800

    Add ajax routes

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,18 @@ Route::get('/about', 'PagesController@about');
 Route::get('/profile', 'PagesController@profile');
 Route::get('/home', 'HomeController');
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+Route::get('/login/ajax',[
+  'as'   => 'login.ajax',
+  'uses' => '\App\Http\Controllers\Auth\LoginController@ajaxShowForm'
+]);
+
+Route::get('/register/ajax',[
+  'as'   => 'register.ajax',
+  'uses' => '\App\Http\Controllers\Auth\RegisterController@ajaxShowForm'
+]);
+
+Route::get('/resetpassword',[
+  'as'   => 'password.reset.ajax',
+  'uses' => '\App\Http\Controllers\Auth\ForgotPasswordController@ajaxShowForm'
+]);
