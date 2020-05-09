5c464e2436dd7aa8ab3cc15db60e60fcfcbc8e02
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-05-09 14:29:39 +0800

    Update profile page grid

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -22,96 +22,94 @@
         </div>
 
 
-      <div class="col-7@md">
-        <ul class="grid-auto-md gap-md">
+      <div class="col-10@md">
+        <div class="margin-top-lg">
+          <ul class="grid-auto-md gap-md">
+            <li><div class="card">
+              <figure class="card__img">
+                <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
+              </figure>
+              <div class="card__content">
+                <div class="text-component">
+                  <h4>Title of the Card</h4>
+                  <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+              </div>
+            </li>
 
-          <li><div class="card">
-            <figure class="card__img">
-              <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
-            </figure>
-            <div class="card__content">
-              <div class="text-component">
-                <h4>Title of the Card</h4>
-                <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <li><div class="card">
+              <figure class="card__img">
+                <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
+              </figure>
+              <div class="card__content">
+                <div class="text-component">
+                  <h4>Title of the Card</h4>
+                  <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
               </div>
-            </div>
-          </li>
-        
-          <li><div class="card">
-            <figure class="card__img">
-              <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
-            </figure>
-            <div class="card__content">
-              <div class="text-component">
-                <h4>Title of the Card</h4>
-                <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li><div class="card">
+              <figure class="card__img">
+                <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
+              </figure>
+              <div class="card__content">
+                <div class="text-component">
+                  <h4>Title of the Card</h4>
+                  <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
               </div>
-            </div>
-          </li>
-        
-          <li><div class="card">
-            <figure class="card__img">
-              <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
-            </figure>
-            <div class="card__content">
-              <div class="text-component">
-                <h4>Title of the Card</h4>
-                <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li><div class="card">
+              <figure class="card__img">
+                <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
+              </figure>
+              <div class="card__content">
+                <div class="text-component">
+                  <h4>Title of the Card</h4>
+                  <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
               </div>
-            </div>
-          </li>
-        
-          <li><div class="card">
-            <figure class="card__img">
-              <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
-            </figure>
-            <div class="card__content">
-              <div class="text-component">
-                <h4>Title of the Card</h4>
-                <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li><div class="card">
+              <figure class="card__img">
+                <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
+              </figure>
+              <div class="card__content">
+                <div class="text-component">
+                  <h4>Title of the Card</h4>
+                  <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
               </div>
-            </div>
-          </li>
-        
-          <li><div class="card">
-            <figure class="card__img">
-              <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
-            </figure>
-            <div class="card__content">
-              <div class="text-component">
-                <h4>Title of the Card</h4>
-                <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li><div class="card">
+              <figure class="card__img">
+                <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
+              </figure>
+              <div class="card__content">
+                <div class="text-component">
+                  <h4>Title of the Card</h4>
+                  <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
               </div>
-            </div>
-          </li>
-        
-          <li><div class="card">
-            <figure class="card__img">
-              <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
-            </figure>
-            <div class="card__content">
-              <div class="text-component">
-                <h4>Title of the Card</h4>
-                <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li><div class="card">
+              <figure class="card__img">
+                <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
+              </figure>
+              <div class="card__content">
+                <div class="text-component">
+                  <h4>Title of the Card</h4>
+                  <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
               </div>
-            </div>
-          </li>
-        
-          <li><div class="card">
-            <figure class="card__img">
-              <img src="{{ asset('assets/img/team-img-5.jpg') }}" alt="Card preview img">
-            </figure>
-            <div class="card__content">
-              <div class="text-component">
-                <h4>Title of the Card</h4>
-                <p class="text-sm color-contrast-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </div>
-            </div>
-          </li>
-        
-        
-        
-        </ul>
+            </li>
+          </ul><!-- /.grid-auto-md gap-md -->
+        </div><!-- /.margin-top-lg -->
       </div>
     </div>
   </div>
