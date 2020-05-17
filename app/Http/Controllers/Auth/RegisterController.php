1e15b71d14a5a26c111f7ed1af95667cd0266a50
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-05-17 21:06:21 +0800

    Fix SQL error on registraion

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+
 use App\User;
+use App\Role;
+
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -68,11 +71,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $permission = Role::where('key', 'subscriber')->first()->permission;
+
         return User::create([
-            'username' => $data['username'],
-            'email'    => $data['email'],
-            'name'     => $data['full_name'],
-            'password' => Hash::make($data['password']),
+            'username'   => $data['username'],
+            'email'      => $data['email'],
+            'name'       => $data['full_name'],
+            'password'   => Hash::make($data['password']),
+            'permission' => $permission
         ]);
     }
 
