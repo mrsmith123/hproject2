874f837fcbff1c899ef0d9ea1f4a616f08f431fa
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-05-17 19:43:42 +0800

    Add middleware for checking user session

---
+++ app/Http/Middleware/CheckUserSession.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckUserSession
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(session('user_id') === null){
+            return redirect('/#login');
+        }
+
+        return $next($request);
+    }
+}
