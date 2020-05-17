7bbcd0394fe6a882776d7d73285ba17a2fa576c5
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-05-17 22:36:52 +0800

    Replace permission middleware to role

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkusersession' => \App\Http\Middleware\CheckUserSession::class,
+        'role' => \App\Http\Middleware\CheckUserRole::class,
     ];
 }
