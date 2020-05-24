5b5229f41cf262be5cd21e620a89816062215091
Youngstar <typham2@gmail.com>
2020-05-24 19:55:18 +0700

    Fix admin menu icons and clean up CSS

--- resources/views/pages/blogadmin.blade.php
+++ resources/views/pages/blogadmin.blade.php
@@ -22,36 +22,45 @@
               </svg>
             </button>
 
-            <button class="btn btn--primary" aria-controls="modal-full-width">Add</button>
-
             <menu class="menu-bar menu-bar--expanded@md js-menu-bar">
-                <li class="menu-bar__item menu-bar__item--trigger js-menu-bar__trigger" role="menuitem" aria-label="More options">
-                  <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 16 16"><circle cx="8" cy="7.5" r="1.5" /><circle cx="1.5" cy="7.5" r="1.5" /><circle cx="14.5" cy="7.5" r="1.5" /></svg>
-                </li>
+
+                <li class="menu-bar__item" aria-controls="modal-full-width" role="menuitem">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><title>pencil</title><rect data-element="frame" x="9.600000000000001" y="9.600000000000001" width="28.799999999999997" height="28.799999999999997" rx="6" ry="6" stroke="none" fill="#0d9eec"></rect><g transform="translate(14.399999999999999 14.399999999999999) scale(0.4)" fill="#ffffff"><path fill="#ffffff" d="M43.7,8.6l-4.3-4.3c-0.9-0.9-2.2-1.5-3.5-1.5c-1.3,0-2.6,0.5-3.5,1.5L5.1,31.5C5,31.6,5,31.7,4.9,31.8 c0,0,0,0.1,0,0.1c0,0,0,0.1,0,0.1l-2,12c-0.1,0.3,0.1,0.6,0.3,0.9c0.2,0.2,0.4,0.3,0.7,0.3c0.1,0,0.1,0,0.2,0l12-2c0,0,0.1,0,0.1,0 c0,0,0.1,0,0.1,0c0.1,0,0.2-0.1,0.3-0.2l27.2-27.2C45.7,13.8,45.7,10.6,43.7,8.6z M9.3,42.2l-3.6-3.6l0.7-4.4l7.3,7.3L9.3,42.2z M30.7,18.7L14.4,35c-0.2,0.2-0.5,0.3-0.7,0.3c-0.3,0-0.5-0.1-0.7-0.3c-0.4-0.4-0.4-1,0-1.4l16.3-16.3c0.4-0.4,1-0.4,1.4,0 S31.1,18.3,30.7,18.7z M37.8,18.8l-8.6-8.6l2.6-2.6l8.6,8.6L37.8,18.8z"></path></g></svg>
+                    <span class="menu-bar__label">Add</span>
+                  </li>
               
                 <li class="menu-bar__item" role="menuitem">
-                  <svg class="icon menu-bar__icon" aria-hidden="true" width="30" height="30" viewBox="0 0 24 24"><title>archive-2</title><g fill="#969696"><path fill="#969696" d="M21,8H3C2.448,8,2,8.447,2,9v14c0,0.553,0.448,1,1,1h18c0.552,0,1-0.447,1-1V9C22,8.447,21.552,8,21,8z M17,15c0,0.553-0.448,1-1,1H8c-0.552,0-1-0.447-1-1v-3h2v2h6v-2h2V15z"></path> <rect x="4" y="4" width="16" height="2"></rect> <rect x="7" width="10" height="2"></rect></g></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>single-folded-content</title><g fill="#bfbfbf"><path fill="#bfbfbf" d="M15,0H2C1.448,0,1,0.448,1,1v22c0,0.552,0.448,1,1,1h20c0.552,0,1-0.448,1-1V8h-7c-0.552,0-1-0.448-1-1V0z M5,17h14v2H5V17z M5,12h14v2H5V12z M11,9H5V7h6V9z"></path> <polygon points="22.414,6 17,6 17,0.586 "></polygon></g></svg>
                   <span class="counter counter--critical counter--docked">4 <i class="sr-only">Notifications</i></span>
                   <span class="menu-bar__label">Draft</span>
                 </li>
               
                 <li class="menu-bar__item" role="menuitem">
-                  <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 16 16"><path d="M2,6v8c0,1.1,0.9,2,2,2h8c1.1,0,2-0.9,2-2V6H2z"></path><path d="M12,3V1c0-0.6-0.4-1-1-1H5C4.4,0,4,0.4,4,1v2H0v2h16V3H12z M10,3H6V2h4V3z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>trash-simple</title><g fill="#bfbfbf"><path fill="#bfbfbf" d="M3,8v15c0,0.552,0.448,1,1,1h16c0.552,0,1-0.448,1-1V8H3z M9,19H7v-6h2V19z M13,19h-2v-6h2V19z M17,19h-2v-6 h2V19z"></path> <path d="M23,4h-6V1c0-0.552-0.447-1-1-1H8C7.447,0,7,0.448,7,1v3H1C0.447,4,0,4.448,0,5s0.447,1,1,1 h22c0.553,0,1-0.448,1-1S23.553,4,23,4z M9,2h6v2H9V2z"></path></g></svg>
                   <span class="counter counter--critical counter--docked">4 <i class="sr-only">Notifications</i></span>
                   <span class="menu-bar__label">Delete</span>
                 </li>
 
                 <li class="menu-bar__item" aria-controls="modal-search">
-                    <svg class="icon menu-bar__icon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24"><title>zoom-2</title><g stroke-linecap="square" stroke-linejoin="miter" stroke-width="1" transform="translate(0.5 0.5)" fill="#969696" stroke="#969696"><line fill="none" stroke-miterlimit="10" x1="22" y1="22" x2="16.4" y2="16.4"></line> <circle fill="none" stroke="#969696" stroke-miterlimit="10" cx="10" cy="10" r="9"></circle></g></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>zoom-2</title><g fill="#bfbfbf"><path fill="#bfbfbf" d="M1.29 19.88l4.37-4.37a9.93 9.93 0 0 1-1.66-5.51c0-5.51 4.49-10 10-10s10 4.49 10 10-4.49 10-10 10a9.93 9.93 0 0 1-5.51-1.66l-4.37 4.37a1 1 0 0 1-1.41 0l-1.42-1.42a1 1 0 0 1 0-1.41z m20.71-9.88c0-4.41-3.59-8-8-8s-8 3.59-8 8 3.59 8 8 8 8-3.59 8-8z"></path></g></svg>
                         <span class="menu-bar__label">Search</span>
                       </g>
                     </svg>
                 </li>
 
+                <li class="menu-bar__item hide@md no-js:is-hidden" aria-controls="sidebar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>ic_menu_24px</title>
+                        <g fill="#bfbfbf">
+                            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+                        </g>
+                    </svg>
+                        <span class="menu-bar__label">Sidebar</span>
+                      </g>
+                    </svg>
+                </li>
+
               </menu>
-
-
-                  
+    
                   <div class="modal modal--search modal--animate-fade flex flex-center padding-md js-modal" id="modal-search">
                     <div class="modal__content width-100% max-width-sm" role="alertdialog" aria-labelledby="modal-search-title" aria-describedby="">
                       <form class="full-screen-search">
@@ -79,8 +88,6 @@
                       </svg>
                     </button>
                   </div>
-
-              <p class="hide@md no-js:is-hidden"><button class="btn btn--primary" aria-controls="sidebar">sidebar</button></p>
 
           </nav>
         </div>
